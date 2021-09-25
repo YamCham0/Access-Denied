7cf93a79b69a978c7b635353605e851d4d15d982
Yamil Chamo <yajochakha@gmail.com>
2021-09-25 09:31:51 -0500

    Confirm methods for the characters

--- script.js
+++ script.js
@@ -1,90 +1,108 @@
 // Assignment Code
 // variables for characters Arrays
 var lowerChar = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 var upperChar = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 var specialChar = [
-  '?',
-  '@',
-  '%',
-  '+',
-  '/',
+  "?",
+  "@",
+  "%",
+  "+",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
+  "!",
+  "#",
+  "$",
+  "^",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
-var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // function to grab user input
 function pwCriteria() {
-  var length = parseInt(prompt("Choose a password length from 8 to 128 characters"))
+  var length = parseInt(
+    prompt("Choose a password length from 8 to 128 characters")
+  );
+  // length validation
+  if (length < 8 || length > 128) {
+    alert("Password needs to be in beetwen 8 to 128");
+    return;
+  }
+
+  //I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  var lwrChoice = confirm("Click ok if you want lower case letters");
+  
+  var upprChoice = confirm("Click ok if you want upper case letters");
+
+  var numChoice = confirm("Click ok if you want numbers");
+
+  var spcChoice = confirm("Click ok if you want special characters");
+
 }
 var generateBtn = document.querySelector("#generate");
 
@@ -94,7 +112,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
